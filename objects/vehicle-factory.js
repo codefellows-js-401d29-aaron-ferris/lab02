628e8b2f8a0b32c296334795528db83ef061bc9a
abferris <abferris@gmail.com>
2019-03-19 23:37:25 -0700

    ready as i'll be tonight

--- objects/vehicle-factory.js
+++ objects/vehicle-factory.js
@@ -1,7 +1,41 @@
 
 'use strict';
 
-function CarFactory() {
+const Vehicle = function (name, wheels){
+  this.name = name;
+  this.wheels = wheels;
+  this.drive = () => {
+    return 'Moving Forward';
+  }
+  this.stop = () => {
+    return 'Stopping';
+  }
+};
+
+function CarFactory(name) {
+  let car = Object.assign(
+    {},
+    {name},
+    {wheels: 4},
+    Vehicle()
+  );
+  return Object.freeze(car)
 }
 
-module.exports = CarFactory;
+function MotorcycleFactory(name) {
+  let motorcycle = Object.assign(
+    {},
+    {name},
+    {wheels: 2},
+    {wheelie},
+    Vehicle()
+  );
+  function wheelie(){
+    return 'Wheee!';
+  }
+
+  return Object.freeze(motorcycle)
+
+}
+
+module.exports = {CarFactory, MotorcycleFactory};
